543efbc86234bfe334bc6b6c79c0e5af61f87ccc
Kotsiubynskyi <turboopera@gmail.com>
2024-09-24 19:37:42 +0300

    Update

--- src/main/java/com/gl/eugene/demo/rest/RatingRestController.java
+++ src/main/java/com/gl/eugene/demo/rest/RatingRestController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -30,6 +31,7 @@ public class RatingRestController {
     }
 
     @GetMapping("/healthcheck")
+    @CrossOrigin
     public ResponseEntity<String> healthcheck() {
         return new ResponseEntity<String>("OK", HttpStatus.OK);
     }
